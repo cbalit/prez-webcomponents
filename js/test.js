204061b0e35aef1bc707b9678cb484a7f786a6d8
Cyril Balit <cbalit@gmail.com>
2014-09-03 12:01:38 +0200

    improve slides

--- js/test.js
+++ js/test.js
@@ -1,6 +1,6 @@
 /******** SHADOW DOM TEST ***************************/
 var shadow = document.querySelector('#shadowHost').createShadowRoot();
-shadow.innerHTML = '<h1>Shadow content</h1>' +
+shadow.innerHTML = '<h1>In Shadow</h1>' +
     '<h2><content select=".title"></content></h2>'+
     '<section><content select="*"></content></section>';
 
@@ -9,7 +9,7 @@ shadow.innerHTML = '<h1>Shadow content</h1>' +
 var t = document.querySelector('#mytemplate1');
 // Populate the src at runtime.
 t.content.querySelector('img').src = 'img/templates_64x64.png';
-t.content.querySelector('#comment').innerHTML = 'img/templates_64x64.png';
+t.content.querySelector('#comment').innerHTML = 'My new comment';
 
 var clone = document.importNode(t.content, true);
 document.querySelector('#forTemplate1').appendChild(clone);
